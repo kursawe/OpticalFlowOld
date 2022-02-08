dc095c354570826f0a6b6230462093f046b83d99
apple <apple@8afbc810.st-andrews.ac.uk>
2022-02-08 18:54:12 +0000

    ouline(tba)

--- conduct_optical_flow.py
+++ conduct_optical_flow.py
@@ -10,7 +10,7 @@ print('optical flow')
 #4.Define image gradients Ix,Iy
 #5.Define error function
 #6.Define velocity(Vx,Vy)
-#7.Each box minimize error function to find Vx,Vy
+#7.Each box use Least Squares Minimization to minimize error function to find Vx,Vy
 #8.Initialize velocity field: set Vx=0.Vy=0,get the value of each gamma
 #9.Compute coefficients of Vx,Vy
 #10.Determine velocity(Vx,Vy)
